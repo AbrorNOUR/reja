ed89d7c7df87f14af9f7f07e3da21c708da49732
AbrorNOUR <abrorjonfayzullaev@gmail.com>
2025-06-07 23:35:27 +0900

    fix: mongodb issue

---
+++ app.js
@@ -0,0 +1,61 @@
+// TEST CODE
+
+// Starting point
+console.log("web serverni boshlash");
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+
+// MongoDB connect
+
+
+
+
+// let user;
+// fs.readFile("database/user.json", "utf8", (err, data) => {
+//     if(err) {
+//         console.log("ERROR:", err);
+//     } else {
+//         user = JSON.parse(data)
+//     }
+// });
+
+// 1: Kirish code   backend serverni qurdik Node.jsda express frame workdan foydalanib web serverni qurdik
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));  
+
+// 2: Session
+
+
+// 3: Views code  backend server ichida  frontend ni qurish 2 xil usuli bor  BSSR
+app.set("views", "views");
+app.set("view engine", "ejs"); // 1: usul tradational usul htmlni qurib olib browserga jonatamz  2: usul single app react
+
+
+// 4: Routing code
+// app.get("/hello", function (req, res) {
+//     res.end(`<h1 style="background: yellow">HELLO WORLD</h1>`);
+// });
+// app.get("/gift", function (req, res) {
+//     res.end(`<h1>Siz sovgalar bolimidasiz</h1>`);
+// });
+
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({test: "success" })
+})
+
+app.get('/develop', (req, res) => {
+    res.render("develop", { user: user });
+});
+
+
+app.get("/", function (req, res) {
+    res.render("reja");
+});
+
+module.exports = app;
+
+
--- server.js
+++ server.js
@@ -1,63 +1,28 @@
-// TEST CODE
-
-// Starting point
-console.log("web serverni boshlash");
-const express = require("express");
-const app = express();
 const http = require("http");
-const fs = require("fs");
+const app = require("./app");
+const mongodb = require("mongodb");
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-    if(err) {
-        console.log("ERROR:", err);
-    } else {
-        user = JSON.parse(data)
-    }
-});
-
-// 1: Kirish code   backend serverni qurdik Node.jsda express frame workdan foydalanib web serverni qurdik
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));  
-
-// 2: Session
+let db;
+const connectionString = "mongodb+srv://Nour:wgc5kKOs958n0VXB@cluster0.cqhoihe.mongodb.net/Reja";
 
 
-// 3: Views code  backend server ichida  frontend ni qurish 2 xil usuli bor  BSSR
-app.set("views", "views");
-app.set("view engine", "ejs"); // 1: usul tradational usul htmlni qurib olib browserga jonatamz  2: usul single app react
-
-
-// 4: Routing code
-// app.get("/hello", function (req, res) {
-//     res.end(`<h1 style="background: yellow">HELLO WORLD</h1>`);
-// });
-// app.get("/gift", function (req, res) {
-//     res.end(`<h1>Siz sovgalar bolimidasiz</h1>`);
-// });
-
-app.post("/create-item", (req, res) => {
-    console.log(req.body);
-    res.json({test: "success" })
-})
-
-app.get('/develop', (req, res) => {
-    res.render("develop", { user: user });
-});
-
-
-app.get("/", function (req, res) {
-    res.render("reja");
-});
-
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function (){
-    console.log(
+mongodb.connect(connectionString, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+}, 
+(err, client) => {
+    if(err) console.log("ERROR onn connection MongoDB");
+    else {
+        console.log("MongoDB connection succeed")
+        // console.log(client);
+        const server = http.createServer(app);
+        let PORT = 3000;        
+        server.listen(PORT, function (){
+            console.log(
         `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
     );
 });
 
 
+    }   
+});
